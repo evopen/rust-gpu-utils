73761424e2066ea38fdaa4c321004f76ed52b50e
evopen <520dhh@gmail.com>
2021-06-29 13:46:48 +0800

    some basic functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use spirv_std::glam::Vec3;
+
+// [0, 1] float rng
+pub fn rng(state: &mut u32) -> f32 {
+    // Condensed version of pcg_output_rxs_m_xs_32_32, with simple conversion to floating-point [0,1].
+    *state = *state * 747796405 + 1;
+    let state = *state;
+    let mut word = ((state >> (state >> 28) + 4) ^ state) * 277803737;
+    word = (word >> 22) ^ word;
+    return word as f32 / 4294967295.0;
+}
+
+pub fn facefoward(n: &Vec3, i: &Vec3) -> Vec3 {
+    match n.dot(*i) < 0.0 {
+        true => *n,
+        false => -*n,
     }
+}
+
+pub fn reflect(i: Vec3, n: Vec3) -> Vec3 {
+    i - 2.0 * n.dot(i) * n
 }
